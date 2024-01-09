f4832d31292c5cc553d08dd4d2da862ece82227a
Tuğsem Saranlı <saranlitugsem@gmail.com>
2024-01-09 14:21:50 +0300

    Change the path of logo

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from '../../api/axios';
 import { login } from '../../api/config';
 import './login.css';
-import Logo from '../../assets/logo.png';
+import Logo from '/assets/logo.png';
 
 const Login = () => {
   const [username, setUserName] = useState('');
