bb920c0f60f7eb13a2bca3d18328d515a411b1d7
Tuğsem Saranlı <saranlitugsem@gmail.com>
2024-01-28 17:21:09 +0300

    update logo

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -4,11 +4,12 @@ import SocialIcon from './SocialIcon';
 import './sidebar.css';
 
 const Sidebar = () => {
-  const width = window.innerWidth < 768;
+  // const width = window.innerWidth < 768;
+  // console.log(width)
 
   return (
     <div className="nav-bar-container">
-      <div className={width ? 'no-display' : 'logo-container'}>
+      <div className="logo-container">
         <img src="/logo.png" alt="logo" width="80" height="80" />
       </div>
       <div className="nav-links-container">
